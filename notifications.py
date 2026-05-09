6055ff1b9f08524171525f9e64bf28249250820c
parkash34 <ohm.parkash34@gmail.com>
2026-05-09 16:25:31 +0200

    Updated it

--- notifications.py
+++ notifications.py
@@ -1,1 +1,10 @@
-h
+import os
+import smtplib
+import time
+import logging
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+
